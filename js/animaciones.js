2fc22d6010bf43c90e16afb780845c5524e0a519
Alesio López <alesiolopez26@gmail.com>
2022-10-25 15:09:29 -0300

    Se modificaron visuales y funcionalidades

---
+++ js/animaciones.js
@@ -0,0 +1,7 @@
+/* 
+    REVISAR PARA PODER APLICAR.
+    YA ESTÁ INSTALADO, HAY QUE VER CÓMO UTILIZAR.
+*/
+/* import anime from 'animejs/lib/anime.es';
+
+const anime = require('animejs'); */
--- js/scroll.js
+++ js/scroll.js
@@ -3,3 +3,17 @@ AOS.init();
 
 /* MODIFICAR NAV AL HACER SCROLL
  */
+/* guardamos en variables los elementos siguientes*/
+const nav = document.querySelector('nav > div'); /* tomamos el nav */
+let prevY = window.scrollY; /* tomamos la posición actual del scroll */
+
+/* detectamos el evento scroll y le decimos qué hacer*/
+window.addEventListener('scroll', function(){
+    if(prevY == window.scrollY){
+        nav.classList.remove('navbar-scroll');
+        prevY = window.scrollY;
+
+    } else {
+        nav.classList.add('navbar-scroll');
+    }
+});
