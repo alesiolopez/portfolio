0392bc61ef5dec485c2eb4dfb9c046f91392f87e
Alesio López <alesiolopez26@gmail.com>
2022-10-31 21:51:19 -0300

    Mejorando responsive

--- js/scroll.js
+++ js/scroll.js
@@ -5,15 +5,18 @@ AOS.init();
  */
 /* guardamos en variables los elementos siguientes*/
 const nav = document.querySelector('nav > div'); /* tomamos el nav */
+const logo = document.querySelector('nav > div > a') /* tomo el logo */
 let prevY = 0; /* tomamos la posición actual del scroll y por defecto lo dejamos en 0*/
 
 /* detectamos el evento scroll y le decimos qué hacer*/
 window.addEventListener('scroll', function(){
     if(prevY == window.scrollY){
         nav.classList.remove('navbar-scroll');
+        logo.classList.remove('logo-scroll');
         prevY = window.scrollY;
 
     } else {
         nav.classList.add('navbar-scroll');
+        logo.classList.add('logo-scroll');
     }
 });
